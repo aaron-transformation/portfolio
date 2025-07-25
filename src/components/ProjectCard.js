97c9d2acdd83d8ef770f062e96bde4e5c5df6e06
aaron-transformation <aarontran2838@gmail.com>
2025-07-25 12:33:30 -0500

    update urls

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgURL, url }) => {
     return (
         <Col sm={6} md={6}>
-            <a href={url}>
+            <a className="proj-link" href={url}>
             <div className="proj-imgbx">
                 <img src={imgURL} />
                 <div className="proj-txtx">
